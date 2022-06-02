8ffcf2c1f05c25fcc44a933e9f7ef888e1de5dc0
mqddabdillah <mqddabdillah@gmail.com>
2022-06-02 15:11:21 +0700

    add number

---
+++ bin/number.dart
@@ -0,0 +1,18 @@
+void main(){
+
+  print('####### Tipe Data Number #######');
+
+  int number1 = 10;
+  double number2 = 10.5;
+
+  print(number1);
+  print(number2);
+  
+  print('####### Num #######');
+
+  num number = 10;
+  print(number);
+
+  number = 10.5;
+  print(number);
+}
