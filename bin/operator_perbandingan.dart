07770734dedd9c89131ed2638f41af66e7bc92d6
mqddabdillah <mqddabdillah@gmail.com>
2022-06-05 02:14:36 +0700

    add operator_perbandingan

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+
+  print('Putri' == 'Putri');
+  print('Putri' != 'Putri');
+  
+}
